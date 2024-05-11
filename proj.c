505aeea0f362ab9bd42e7db844f75ae800655e2b
Alice <alicecania@gmail.com>
2024-05-11 03:35:00 +0300

    milestone 1 done

--- proj.c
+++ proj.c
@@ -1,116 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
 #include <fcntl.h>
-#include <pwd.h>
-#include <grp.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
 
-#define MAX_FILENAME_LENGTH 256 
+void openDirectories(char *directorPath)
+{
+    DIR *director=opendir(directorPath);
 
-typedef struct Snapshot {
-    char filename[MAX_FILENAME_LENGTH];
-    mode_t permissions;
-    uid_t owner;
-    gid_t group;
-    int size;
-    time_t last_modified;
-} Snapshot;
-
-void create_snapshot(Snapshot snap, FILE* output) {
-    char permission_string[11];
-
-    snprintf(permission_string, sizeof(permission_string), "%c%c%c%c%c%c%c%c%c%c",
-        (S_ISDIR(snap.permissions)) ? 'd' : '-',
-        (snap.permissions & S_IRUSR) ? 'r' : '-',
-        (snap.permissions & S_IWUSR) ? 'w' : '-',
-        (snap.permissions & S_IXUSR) ? 'x' : '-',
-        (snap.permissions & S_IRGRP) ? 'r' : '-',
-        (snap.permissions & S_IWGRP) ? 'w' : '-',
-        (snap.permissions & S_IXGRP) ? 'x' : '-',
-        (snap.permissions & S_IROTH) ? 'r' : '-',
-        (snap.permissions & S_IWOTH) ? 'w' : '-',
-        (snap.permissions & S_IXOTH) ? 'x' : '-');
-
-    struct passwd *pwd;
-    struct passwd *grp;
-    pwd = getpwuid(snap.owner);
-    grp = getgrgid(snap.group);
-
-    fprintf(output, "Filename: %s\n", snap.filename);
-    fprintf(output, "Permissions: %s\n", permission_string);
-    fprintf(output, "Owner: %s\n", (pwd != NULL) ? pwd->pw_name : "Unknown");
-    fprintf(output, "Group: %s\n", (grp != NULL) ? grp->pw_name : "Unknown"); 
-    fprintf(output, "Size: %d bytes\n", snap.size);
-    fprintf(output, "Last Modified: %ld\n", snap.last_modified);
-    fprintf(output, "\n");
-
-    fclose(output);
-}
-
-void parseDir(char path[]) {
-
-    int contor = 0;
-    DIR *new_dir = opendir(path);
-    
-    if(new_dir == NULL) { 
-        perror("Could not open the directory\n");
-        exit(1);
+    if(director==NULL)
+    {
+        printf("Acesta este un mesaj de eroare");
+        return;
     }
 
-    char next_path[4096];
-    struct dirent *dir_data;
+    struct dirent *entry;
 
-    while( (dir_data = readdir(new_dir)) ) { 
 
-        if( (!strcmp(dir_data -> d_name, ".")) || (!strcmp(dir_data -> d_name, "..")) )
-            continue;
-        
-        strcpy(next_path, path);
-        strcat(next_path, "/");
-        strcat(next_path, dir_data -> d_name); 
+    while ((entry = readdir(director)) != NULL)
+    {
+        struct stat buf;
+        char path[1024];
+        char filename[1024]="";
 
-        struct stat st;
-        if( lstat(next_path, &st) < 0 ) {
-            perror("Could not access the current file (bad path)\n");
-            exit(2);
+        switch (entry->d_type)
+        {
+
+            case DT_DIR:
+                if (!(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0))
+                {
+                     snprintf(path, sizeof(path), "%s/%s", directorPath, entry->d_name);
+                     openDirectories(path);
+                }
+                break;
+
+            case DT_REG:   
+                sprintf(filename,"%s/%s",directorPath,entry->d_name);
+
+                char snapshotFilename[1024]="";
+                sprintf(snapshotFilename, "%s.snapshot", filename);
+                
+
+                if(stat(filename, &buf) < 0)
+                {
+                    printf("Error stat2 \n");
+                    exit(-1);
+                }
+
+                int fptr;
+
+                fptr = open(snapshotFilename, O_CREAT | O_WRONLY | O_RDONLY);
+
+                char snapshotContent[1024] = "";
+
+                sprintf(snapshotContent, "Entry: %s\nSize: %d\nInode number: %d\n", filename, buf.st_size, buf.st_ino);
+
+                ssize_t result = write(fptr, &snapshotContent, strlen(snapshotContent) - 1);
+
+                // printf("Timestamp: %s\n", buf.st_atimespec.tv_sec);
+                            /// printf("Entry: %s\n", filename);
+                            ///printf("Size: %d\n", buf.st_size);
+                            // printf("Last Modified: %s\n",  buf.st_mtimespec.tv_sec);
+                            // printf("Permissions: %s\n", filename);
+                            ///printf("Inode number: %d\n", buf.st_ino);
+
+                close(fptr);
+               
+                break;
+
         }
-
-        Snapshot snap;
-        strcpy(snap.filename, next_path);
-        snap.permissions = st.st_mode;
-        snap.owner = st.st_uid;
-        snap.group = st.st_gid;
-        snap.size = st.st_size;
-        snap.last_modified = st.st_mtime;
-
-        if(S_IFDIR == (st.st_mode & S_IFMT))
-            parseDir(next_path); 
-        else
-            create_snapshot(snap, output);
     }
 
-    closedir(new_dir);
+    closedir(director);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "argument error\n");
-        return 1;
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage <exec> dir N\n");
+        exit(-1);
     }
 
-     FILE* output = fopen("Snapshot.txt", "w");
-    if (!output) {
-        perror("error opening file\n");
-        return 1;
-    }
-
-    parseDir(argv[1]);
-
-    fclose(output);
+    openDirectories(argv[1]);
 
     return 0;
 }
